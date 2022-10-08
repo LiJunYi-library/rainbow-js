6c19c4a544098fc3a5f5e1a0d63422e172dce273
LiJunYi <1040381245@qq.com>
2022-10-08 16:35:25 +0800

    no message

--- proxy/PROXY.d.ts
+++ proxy/PROXY.d.ts
@@ -4,21 +4,6 @@ type BaseVue = any;
 
 type Fun = (...arg: any) => any;
 
-export declare type ApplayInstance = {
-  (...params: any | undefined): any;
-  onApply?: Fun;
-  afterApply?: Fun;
-  onApplyListener?: Fun[];
-  afterApplyListener?: Fun[];
-  addOnApplyListener: (fun: Fun) => void;
-  removeOnApplyListener: (fun: Fun) => void;
-  addAfterApplyListener: (fun: Fun) => void;
-  removeAfterApplyListener: (fun: Fun) => void;
-};
-
-export declare function applay(func: Fun, target?: any,): ApplayInstance
-
-
 export declare type ApplyInstance = {
   (...params: any | undefined): any;
   onApply?: Fun;
@@ -32,5 +17,12 @@ export declare type ApplyInstance = {
 };
 
 export declare function apply(func: Fun, target?: any,): ApplyInstance
+
+export declare type ApplayInstance = ApplyInstance;
+
+export declare function applay(func: Fun, target?: any,): ApplayInstance
+
+
+
 
 
