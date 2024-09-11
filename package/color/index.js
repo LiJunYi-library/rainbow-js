10ce62c4f7cc72a8af66a2cff3d0c2b69d49406d
lijunyi <1040381245@qq.com>
2024-09-11 19:47:14 +0800

    颜色过渡方法

--- package/color/index.js
+++ package/color/index.js
@@ -39,3 +39,18 @@ export function transitionStyleColor(...arg) {
   const trColor = transitionColor(...arg);
   return (ratio) => `color: ${trColor(ratio)};`;
 }
+
+
+export function transitionOpacity(def, tart) {
+  const gaps = tart - def;
+  return ratio => {
+      if (ratio > 1) ratio = 1;
+    const opacity = gaps * ratio + def;
+    return opacity;
+  };
+}
+
+export function transitionStyleOpacity(...arg) {
+  const trColor = transitionOpacity(...arg);
+  return ratio => `opacity: ${trColor(ratio)};`;
+}
